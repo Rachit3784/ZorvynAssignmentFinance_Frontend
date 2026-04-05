9adc13d3126ca714b3b0183fd46a08518dd81006
Rachit3784 <grachit736@gmail.com>
2026-04-05 13:59:51 +0530

    update url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8900/api',
+    baseURL: 'https://zorvynassignmentfinance-backend.onrender.com/api',
 });
 
 // Add a request interceptor
